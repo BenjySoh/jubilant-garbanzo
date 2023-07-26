6bc19ccb571127ed7a1255fc1f286625b10bd7a5
BenjyS <123656145+BenjySoh@users.noreply.github.com>
2023-07-26 11:50:54 +0800

    Update filemanipulation.py
    v2

--- filemanipulation.py
+++ filemanipulation.py
@@ -1,9 +1,10 @@
 import os
 filename = input('enter file name (.txt): ')
 if os.path.exists(filename):
-    openfile = open(filename, 'r')
+    openfile = open(filename, 'r') #Precise accessibility, edit open(filename, 'path', 'r') 
     for count, ofl in enumerate(openfile):
         print(f"\nentry({count}): {ofl}")
+    count += 1 #due to "forloop" started on index "0". 
     print(f"\nnumber of entry: {count}")
 else:
     print("file not found, please check and try again.")
